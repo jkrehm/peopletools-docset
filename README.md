f1bf9907ea4afadeacaf4065cd1c3fe7280c14f9
Jonathan Rehm <jonathan@rehm.me>
2018-02-09 11:41:04 -0700

    Make various changes (see below)
    * Clear docs before each run
    * Update index page
    * Do not manually produce Resources.db (Velocity will do that itself)

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ To use:
 
 1. Run `yarn install`
 2. Ensure `src/docs.json` is up-to-date
+3. Ensure `dashIndexFilePath` in `Info.plist` is up-to-date
 3. Run `yarn run generate`
 
 The script will produce `PeopleTools.tgz`.
--- src/build.js
+++ src/build.js
@@ -4,24 +4,19 @@ const mkdirp = require('mkdirp');
 const path = require('path');
 const pretty = require('pretty');
 const request = require('request-promise');
+const rimraf = require('rimraf');
 const sqlite3 = require('sqlite3');
-const zlib = require('zlib');
 const { sleep } = require('./util');
 const docs = require('./docs');
 
 const resources = `${__dirname}/../PeopleTools.docset/Contents/Resources`;
 const docSetDB = new sqlite3.Database(`${resources}/docSet.dsidx`);
-const resourcesDB = new sqlite3.Database(`${resources}/resources.db`);
 
+// Clean up
+rimraf.sync(`${resources}/Documents/cd`);
 docSetDB.exec('delete from SearchIndex');
-resourcesDB.exec('delete from FilePaths');
-resourcesDB.exec('delete from Files');
 
 const insertIndex = docSetDB.prepare('insert into SearchIndex (name, type, path) values(?, ?, ?)');
-const insertFilePath = resourcesDB.prepare('insert into FilePaths (FileKey, Path) values(?, ?)');
-const insertFile = resourcesDB.prepare('insert into Files (Key, Content) values(?, ?)');
-
-let counter = 1;
 
 const getDocs = async ([type, urls]) => {
   for (let i = 0; i < urls.length; i++) {
@@ -86,11 +81,6 @@ const getDocs = async ([type, urls]) => {
 
     const html = pretty($.html(), { ocd: true });
 
-    // Insert file into database
-    insertFilePath.run(counter, `Documents/${url}`, err => err && console.error(err, url));
-    insertFile.run(counter, zlib.gzipSync(html), err => err && console.error(err, url));
-    counter++;
-
     // Export to file
     const filePath = `${resources}/Documents/${url}`;
     mkdirp.sync(path.dirname(filePath));
@@ -99,11 +89,6 @@ const getDocs = async ([type, urls]) => {
     await sleep(1000);
   }
 };
-
-// Add styles
-insertFilePath.run(counter, 'Documents/style.css');
-insertFile.run(counter, zlib.gzipSync(fs.readFileSync(`${resources}/Documents/style.css`)));
-counter++;
 
 // Generate docs
 Object.entries(docs).forEach(getDocs);
